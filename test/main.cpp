78a15098a0d1c54f59f3f4063c3243fa6268a154
John Schneider <johnschneider@gmx.com>
2020-12-06 13:14:18 -0600

    Added make test funcionality to makefile

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string>
+
+#include "testKick.hpp"
+
+int main(int argc, char **argv)
+{
+    testKick();  
+    return 0;  
+}
---
+++ test/testKick.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+
+static void test1()
+{
+    std::cout << "test1";
+}
+void testKick()
+{
+    test1();
+}
