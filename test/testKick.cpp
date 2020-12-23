505ba9d65db5b4b681c050d06b0f023da23a7f80
John Schneider <johnschneider@gmx.com>
2020-12-23 10:22:38 -0600

    switched to new branch laptop. seg fault when trying to access pluginInstance

--- test/testKick.cpp
+++ test/testKick.cpp
@@ -1,14 +1,23 @@
 #include "NewMacros.h"
 #include "CppUTest/TestHarness.h"
 
+
 TEST_GROUP(FirstTestGroup)
 {
-
+    void setup()
+    {
+        Plugin *p = new Plugin();
+        init(p);
+    }
+    void teardown()
+    {
+    }
 };
 
 TEST(FirstTestGroup, SecondTest)
 {
-    FAIL("Intentional Fail");
+    
+    Model *modelKick = pluginInstance->getModel("Kick");
     // bool b = modelKick == 0;
     // CHECK(b);
 }
